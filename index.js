494ae5b2bdf460c8adff1d776c1073b6f9a717d1
hujimiya <hujimiya@outlook.com>
2017-03-01 21:06:05 +0800

    try show env.TEST(should be 233)

--- index.js
+++ index.js
@@ -3,10 +3,11 @@
  */
 var http = require("http");
 var port = process.env.PORT || 6565;
+
 http.createServer(function (request,resp) {
     resp.writeHead(200,{
        "Content-Type":"text/plain"
     });
-    resp.end("Hello Heroku");
+    resp.end("Hello Heroku"+"env.TEST:"+process.env.TEST);
 }).listen(port);
 console.log("Running at ",port);
