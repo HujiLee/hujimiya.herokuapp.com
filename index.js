eb9c6602e68a65822a721eb02274cc701ee85c56
hujimiya <hujimiya@outlook.com>
2017-03-01 20:56:19 +0800

    add index.js 看看能不能在Heroku运行

---
+++ index.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Administrator on 2017/3/1 0001.
+ */
+var http = require("http");
+var port = process.env.PORT || 6565;
+http.createServer(function (request,resp) {
+    resp.writeHead(200,{
+       "Content-Type":"text/plain"
+    });
+    resp.end("Hello Heroku");
+}).listen(port);
+console.log("Running at ",port);
