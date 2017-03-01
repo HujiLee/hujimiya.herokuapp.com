a0a47d710e09e30e5673ad151642a7f9a956c4ea
hujimiya <hujimiya@outlook.com>
2017-03-01 23:29:49 +0800

    start render tsdm

--- routes/tsdm.js
+++ routes/tsdm.js
@@ -5,8 +5,17 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/',function (req,res) {
-    var MONGOURL = process.env.MONGOURL||"local "+require("../mongodb-info.json").url;
-    res.send(["TSDM","Download","Area",MONGOURL])
+    //仅仅用来测试本地或Heroku端是否都能正常得到mongodb的url
+   /* var MONGOURL = process.env.MONGOURL||"local "+require("../mongodb-info.json").url;
+    res.send(["TSDM"
+        ,"Download"
+        ,"Area"
+        ,MONGOURL
+    ]);*/
+   res.render("tsdm",{
+       title:"Welcome to TSDM 下载区版头编辑系统"
+   });
+
 });
 
 
